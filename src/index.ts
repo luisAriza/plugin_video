64e2f219ff39ca490c98892fe431308b96c3e29c
Luis Ariza <luismanuelariza@hotmail.com>
2022-06-09 10:34:08 -0500

    Migracion del proyecto completo a TypeScript

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import MediaPlayer from "./app/MediaPlayer";
+import AutoPlay from "./app/plugins/AutoPlay";
+import AutoPause from "./app/plugins/AutoPause";
+
+const video = document.querySelector("video");
+const player = new MediaPlayer({
+    elm: video,
+    plugins: [new AutoPlay(), new AutoPause()],
+});
+
+const playButton: HTMLElement = document.querySelector("#playButton");
+playButton.onclick = () => player.togglePlay();
+
+const muteButton: HTMLElement = document.querySelector("#muteButton");
+muteButton.onclick = () => {
+  if (player.media.muted) {
+    player.unmute();
+  } else {
+    player.mute();
+  }
+};
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/sw.js").catch((error) => {
+    console.log(error.message);
+  });
+}
